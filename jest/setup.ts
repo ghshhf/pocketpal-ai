0d0b72988113bae57f01a91d4e3e224f5940f025
a-ghorbani <ghorbani59@gmail.com>
2026-06-11 14:11:22 +0200

    chore: drop deviceRulesStore test mock and registration
    The mock store and its global jest registration referenced the removed
    DeviceRulesStore; remove them to complete the deletion closure.

--- jest/setup.ts
+++ jest/setup.ts
@@ -57,7 +57,6 @@ import {deepLinkStore as mockDeepLinkStore} from '../__mocks__/stores/deepLinkSt
 import {mockServerStore} from '../__mocks__/stores/serverStore';
 import {mockTTSStore} from '../__mocks__/stores/ttsStore';
 import {checkoutFlowStore as mockCheckoutFlowStore} from '../__mocks__/stores/checkoutFlowStore';
-import {mockDeviceRulesStore} from '../__mocks__/stores/deviceRulesStore';
 
 jest.mock('@react-native-clipboard/clipboard', () => mockClipboard);
 
@@ -116,7 +115,6 @@ jest.mock('../src/store', () => {
     serverStore: mockServerStore,
     ttsStore: mockTTSStore,
     checkoutFlowStore: mockCheckoutFlowStore,
-    deviceRulesStore: mockDeviceRulesStore,
     defaultCompletionSettings: mockDefaultCompletionSettings,
   };
 });
